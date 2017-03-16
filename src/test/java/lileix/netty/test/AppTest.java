f2b16e31abae9577aa5d578a1423b79e98d9ba3c
lilei <37665049@qq.com>
2017-03-17 00:42:24 +0800

    Initial commit

---
+++ src/test/java/lileix/netty/test/AppTest.java
@@ -0,0 +1,23 @@
+package lileix.netty.test;
+
+import io.netty.util.internal.SystemPropertyUtil;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Created by lilei on 2017/3/17.
+ */
+public class AppTest {
+    public static final String HTTP_DATE_FORMAT = "EEE, dd MMM yyyy HH:mm:ss zzz";
+    public static final String HTTP_DATE_GMT_TIMEZONE = "GMT";
+    static SimpleDateFormat dateFormatter = new SimpleDateFormat(HTTP_DATE_FORMAT, Locale.US);
+
+    public static void main(String[] args) {
+        String path = SystemPropertyUtil.get("user.dir");
+        System.out.println(path);
+        System.out.println(System.getProperty("user.dir"));
+        System.out.println(dateFormatter.format(new Date()));
+    }
+}
